e038852087f5f9d590b02e92dcbec1134da57e2f
Ilya <kotyrov.1310@gmail.com>
2023-02-25 15:22:34 +0100

    implement basic tab switch logic

--- src/script.js
+++ src/script.js
@@ -1,17 +1,42 @@
 import './styles/style.scss';
 import { todoFactory } from './todo.js';
 import { projectFectory } from './project.js';
+import { tabFactory } from './tabFactory';
 
 const contentContaier = document.getElementById('content-container');
 
-function switchTab(tabFactory) {
+const allBtn = document.getElementById('nav-all');
+const todayBtn = document.getElementById('nav-today');
+const weekBtn = document.getElementById('nav-week');
+const completedbtn = document.getElementById('nav-completed');
+
+switchTab(tabFactory('All tasks'), allBtn);
+
+allBtn.addEventListener('click', () => {
+	switchTab(tabFactory('All tasks'), allBtn);
+});
+todayBtn.addEventListener('click', () => {
+	switchTab(tabFactory('Tasks for today'), todayBtn);
+});
+weekBtn.addEventListener('click', () => {
+	switchTab(tabFactory('Tasks for this week'), weekBtn);
+});
+completedbtn.addEventListener('click', () => {
+	switchTab(tabFactory('Completed tasks'), completedbtn);
+});
+
+function switchTab(tab, navBtn) {
 	clearContent();
-	const content = tabFactory();
-	contentContaier.appendChild(content);
+	contentContaier.appendChild(tab);
+	navBtn?.classList.add('open');
 }
 
 function clearContent() {
 	while (contentContaier.lastChild) {
 		contentContaier.removeChild(contentContaier.lastChild);
+	}
+	const openNavBtns = document.getElementsByClassName('open');
+	for (let btn of openNavBtns) {
+		btn.classList.remove('open');
 	}
 }
