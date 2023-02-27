6145d49f40abf10688ca5d07f0dfa78051e0be5a
Ilya <kotyrov.1310@gmail.com>
2023-02-27 17:57:03 +0100

    add new task button

--- src/tabFactory.js
+++ src/tabFactory.js
@@ -12,9 +12,18 @@ export default function tabFactory(tabName, todoList = []) {
 	header.appendChild(divider);
 	content.appendChild(header);
 
+	// todo list
+
 	const list = document.createElement('div');
 	list.classList.add('todo-list');
 	content.appendChild(list);
+
+	const addItem = document.createElement('button');
+	addItem.classList.add('add-item-btn');
+	const addIcon = document.createElement('svg');
+	addItem.innerHTML =
+		'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.2" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /> </svg><h4>Add task</h4>';
+	list.appendChild(addItem);
 
 	for (let todo of todoList) {
 		list.appendChild(todoItemFactory(todo));
